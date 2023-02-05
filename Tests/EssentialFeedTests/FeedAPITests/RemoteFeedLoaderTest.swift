dc8d3a467cac78203d283f7179c9ee0b098462cb
Brent Mifsud <18543934+BrentMifsud@users.noreply.github.com>
2023-02-05 14:55:44 -0500

    add complete with error method to client

--- Tests/EssentialFeedTests/FeedAPITests/RemoteFeedLoaderTest.swift
+++ Tests/EssentialFeedTests/FeedAPITests/RemoteFeedLoaderTest.swift
@@ -22,6 +22,10 @@ final class RemoteFeedLoaderTest: XCTestCase {
             completions.append(completion)
             requestedURLs.append(url)
         }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
+        }
     }
     
     func test_init_doesNotRequestDataFromURL() {
@@ -52,7 +56,7 @@ final class RemoteFeedLoaderTest: XCTestCase {
         sut.load { capturedErrors.append($0) }
         
         let clientError = NSError(domain: "Test", code: 0)
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
