f74cf6638e0ddd066dc3aa7a615e8fd3f5c9c945
Brent Mifsud <18543934+BrentMifsud@users.noreply.github.com>
2023-02-04 23:23:04 -0500

    Add FeedItem

---
+++ Sources/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//
+//
+//  Created by Brent Mifsud on 2023-02-04.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let image: URL
+}
