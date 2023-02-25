265ae499ed3c3339a0c074dbb2f4be9a3ce0773e
Brent Mifsud <18543934+BrentMifsud@users.noreply.github.com>
2023-02-25 14:08:43 -0500

    refactor url session mocking strategy with URLProtocol stubs to remove test coupling with url session types

--- Tests/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ Tests/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -9,9 +9,9 @@ import XCTest
 import EssentialFeed
 
 class URLSessionHTTPClient: HTTPClient {
-	private let session: HTTPSession
+	private let session: URLSession
 	
-	init(session: HTTPSession) {
+	init(session: URLSession = .shared) {
 		self.session = session
 	}
 	
@@ -25,40 +25,28 @@ class URLSessionHTTPClient: HTTPClient {
 	}
 }
 
-protocol HTTPSession {
-	func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> HTTPSessionTask
-}
-
-protocol HTTPSessionTask {
-	func resume()
-}
-
 final class URLSessionHTTPClientTests: XCTestCase {
-	func test_getFromURL_resumesDataTaskWithURL() {
-		let url = URL(string: "http://any-url.com")!
-		let session = HTTPSessionSpy()
-		let task = HTTPSessionTaskSpy()
-		session.stub(url: url, task: task)
-		let sut = URLSessionHTTPClient(session: session)
-		sut.get(from: url) { _ in }
-		
-		XCTAssertEqual(task.resumeCallCount, 1)
+	override func setUp() {
+		URLProtocolStub.startInterceptingRequests()
+	}
+	
+	override func tearDown() {
+		URLProtocolStub.stopInterceptingRequests()
 	}
 	
 	func test_getFromURL_failsOnRequestError() {
 		let url = URL(string: "http://any-url.com")!
 		let error = NSError(domain: "Test", code: 1)
-		let session = HTTPSessionSpy()
-		let task = HTTPSessionTaskSpy()
-		session.stub(url: url, task: task, error: error)
-		let sut = URLSessionHTTPClient(session: session)
+		URLProtocolStub.stub(url: url, error: error)
+		let sut = URLSessionHTTPClient()
 		
 		let exp = expectation(description: "Wait for completion")
 		
 		sut.get(from: url) { result in
 			switch result {
-			case let .failure(recievedError as NSError):
-				XCTAssertEqual(error, recievedError)
+			case let .failure(receivedError as NSError):
+				XCTAssertEqual(receivedError.domain, error.domain)
+				XCTAssertEqual(receivedError.code, error.code)
 			default:
 				XCTFail("Expected failure with error \(error), got \(result) instead")
 			}
@@ -72,33 +60,49 @@ final class URLSessionHTTPClientTests: XCTestCase {
 
 // MARK: - Helpers
 
-private class HTTPSessionSpy: HTTPSession {
+private class URLProtocolStub: URLProtocol {
 	private struct Stub {
-		let task: HTTPSessionTask
 		let error: Error?
 	}
 	
-	private var stubs = [URL: Stub]()
+	private static var stubs = [URL: Stub]()
 	
-	
-	func stub(url: URL, task: HTTPSessionTask, error: Error? = nil) {
-		stubs[url] = Stub(task: task, error: error)
+	static func stub(url: URL, error: Error? = nil) {
+		stubs[url] = Stub(error: error)
 	}
 	
-	func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> HTTPSessionTask {
-		guard let stub = stubs[url] else {
-			fatalError("Couldn't find stub for \(url)", file: #filePath, line: #line)
+	static func startInterceptingRequests() {
+		URLProtocol.registerClass(URLProtocolStub.self)
+	}
+	
+	static func stopInterceptingRequests() {
+		URLProtocol.unregisterClass(URLProtocolStub.self)
+		stubs = [:]
+	}
+	
+	override class func canInit(with request: URLRequest) -> Bool {
+		guard let url = request.url else {
+			return false
 		}
 		
-		completionHandler(nil, nil, stub.error)
-		return stub.task
+		return stubs[url] != nil
 	}
-}
-
-private class HTTPSessionTaskSpy: HTTPSessionTask {
-	var resumeCallCount: Int = 0
 	
-	func resume() {
-		resumeCallCount += 1
+	override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+		return request
 	}
+	
+	override func startLoading() {
+		guard let url = request.url, let stub = Self.stubs[url] else {
+			return
+		}
+		
+		if let error = stub.error {
+			client?.urlProtocol(self, didFailWithError: error)
+		}
+		
+		client?.urlProtocolDidFinishLoading(self)
+	}
+	
+	override func stopLoading() {}
 }
