172f7a0e2dfad2f8511b96a3dfb7144e020cfecd
Brent Mifsud <18543934+BrentMifsud@users.noreply.github.com>
2023-02-26 00:00:22 -0500

    remove package.resolved

--- Package.swift
+++ Package.swift
@@ -21,10 +21,7 @@ let package = Package(
 			name: "EssentialFeedTestHelpers",
 			targets: ["EssentialFeedTestHelpers"]),
 	],
-	dependencies: [
-		// Dependencies declare other packages that this package depends on.
-		 .package(url: "https://github.com/apple/swift-format.git", from: "0.50700.1"),
-	],
+	dependencies: [],
 	targets: [
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
 		// Targets can depend on other targets in this package, and on products in packages this package depends on.
